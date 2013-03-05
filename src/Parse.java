c9dff25710d83cde153c57520942467110da7c95
s1207970 <s1207970@prouvaire.inf.ed.ac.uk>
2013-03-05 11:57:51 +0000

    Working Version - Downloads file and then parses, command line output

---
+++ src/Parse.java
@@ -0,0 +1,97 @@
+// Roy's XML Parser. :)
+import java.io.IOException;
+import javax.xml.parsers.*;
+import javax.xml.xpath.*;
+import org.w3c.dom.*;
+import org.xml.sax.SAXException;
+import java.net.*;
+import java.io.*;
+
+public class Parse {
+
+    public static void main(String[] args)
+            throws ParserConfigurationException, SAXException,
+            IOException, XPathExpressionException {
+
+
+        download();
+
+
+        DocumentBuilderFactory domFactory = DocumentBuilderFactory.newInstance();
+        domFactory.setNamespaceAware(true); // important line
+        DocumentBuilder builder = domFactory.newDocumentBuilder();
+        Document doc = builder.parse("bookupdate.xml");
+
+        XPathFactory factory = XPathFactory.newInstance();
+        XPath xpath = factory.newXPath();
+        // experimental XPATH query that will extract URL's
+        // seems to work...
+        
+        
+        XPathExpression expr = xpath.compile("//item/enclosure/@url"); // XPATH QUERY. 
+
+
+
+        Object result = expr.evaluate(doc, XPathConstants.NODESET);
+        NodeList nodes = (NodeList) result;
+        
+        System.out.println("Parsed XML file sucessfully.. Displaying Results");
+        
+        for (int i = 0; i < nodes.getLength(); i++) {
+            System.out.println(nodes.item(i).getNodeValue());
+            // OUTPUT urls.
+        }
+
+            
+
+
+
+
+
+
+    }
+
+    public static void download() {
+        try {
+            /*
+             * Get a connection to the URL and start up
+             * a buffered reader.
+             */
+            long startTime = System.currentTimeMillis();
+
+            System.out.println("Connecting to NYT servers \n");
+
+            URL url = new URL("http://www.nytimes.com/services/xml/rss/nyt/podcasts/bookupdate.xml");
+            url.openConnection();
+            InputStream reader = url.openStream();
+
+            /*
+             * Setup a buffered file writer to write
+             * out what we read from the website.
+             */
+            FileOutputStream writer = new FileOutputStream("bookupdate.xml");
+            byte[] buffer = new byte[153600];
+            int totalBytesRead = 0;
+            int bytesRead = 0;
+
+            System.out.println("Reading XML file 150KB blocks at a time.\n");
+
+            while ((bytesRead = reader.read(buffer)) > 0) {
+                writer.write(buffer, 0, bytesRead);
+                buffer = new byte[153600];
+                totalBytesRead += bytesRead;
+            }
+
+            long endTime = System.currentTimeMillis();
+
+            System.out.println("Done. " + (new Integer(totalBytesRead).toString()) + " bytes read (" + (new Long(endTime - startTime).toString()) + " millseconds).\n");
+            writer.close();
+            reader.close();
+        } catch (MalformedURLException e) {
+            e.printStackTrace();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
+    }
+}
